61859ccf18e7141cbae6c51f53f3334443925748
pavani.p <146440205+pavani19995@users.noreply.github.com>
2023-12-12 11:21:09 +0530

    Update Customer_Satisfaction.py

--- Customer_Satisfaction.py
+++ Customer_Satisfaction.py
@@ -157,7 +157,11 @@ def Customer_Satisfaction():
             #st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
             #st.subheader("Overall NPS Result by CustomerID")
             pd_df_nps_cust_wid2=pd_df_nps_cust_wid.groupby(['Overall_NPS_Result']) .apply(lambda x: pd.Series({'CUSTOMER_COUNT' : x['customerid'].count()}))
-            pd_df_nps_cust_wid2=pd_df_nps_cust_wid2.reset_index(0)
+            if not pd_df_nps_cust_wid2.empty:
+                pd_df_nps_cust_wid2 = pd_df_nps_cust_wid2.reset_index(0)
+            else:
+                pd_df_nps_cust_wid2 = pd.DataFrame(columns=['Overall_NPS_Result', 'Count of Customers'])
+
             pd_df_nps_cust_wid2.rename(columns = {'Overall_NPS_Result':'Overall_NPS_Result', 'CUSTOMER_COUNT':'Count of Customers'}, inplace = True)
             fig = px.pie(pd_df_nps_cust_wid2, names='Overall_NPS_Result', values='Count of Customers', hole = 0.65, color='Overall_NPS_Result',
                     color_discrete_map={'promote':'#0CC368',
